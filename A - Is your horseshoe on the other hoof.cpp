7221b5d88ca402cbc37e21820d76cc8f2bb8bb5a
Abodahab <medoabodahab@gmail.com>
2023-10-26 03:25:02 +0200

    new

---
+++ A - Is your horseshoe on the other hoof.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include<string.h>
+#include<algorithm>
+int main(){
+       long long int arr[4];
+
+       int counter=0;
+       for(int i=0; i<4; i++)
+       {
+         cin>>arr[i];
+         sort(arr,arr+4);
+        }
+
+       for(int i=0;i<4;++i)
+       {
+           if(arr[i]==arr[i+1])
+               counter ++;
+       }
+       cout<<counter;
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
